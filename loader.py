fe431325946d51b5fea592396d8e44c250df0ab9
kapselccc <s184848@student.pg.edu.pl>
2022-04-05 21:28:25 +0200

    Editing loader and adding dijkstra algorithm

--- loader.py
+++ loader.py
@@ -3,24 +3,26 @@ import csv
 import networkx as nx
 from matplotlib import pyplot as plt
 
+
 def load_graph():
     graph = nx.Graph()
     with open('./graph_input.csv', encoding='utf-8') as file_graph_input:
         csv_reader = csv.reader(file_graph_input, delimiter=' ')
 
         for row in csv_reader:
-            initial_node = row[0]
+            initial_node = int(row[0])
 
             for node in row[1:]:
-                graph.add_edge(initial_node, node)
+                graph.add_edge(initial_node, int(node))
 
     return graph
 
-G = load_graph()
-for node in nx.nodes(G):
-    ns = G.neighbors(node)
-    print(f"{node}: ", end='')
-    for n in ns:
-        print(n, end=' ')
+def show_graph():
+    G = load_graph()
+    for node in nx.nodes(G):
+        ns = G.neighbors(node)
+        print(f"{node}: ", end='')
+        for n in ns:
+            print(n, end=' ')
 
-    print("")
+        print("")
