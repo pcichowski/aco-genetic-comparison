57b11d0cd45f167ff724bb240b62e5c772f296ae
Paweł Cichowski <81694867+Silentsky0@users.noreply.github.com>
2022-04-09 14:00:56 +0200

    Separated ants into basic and extended algorithms

--- Ant.py
+++ Ant.py
@@ -2,8 +2,8 @@ import numpy as np
 from numpy.random import choice
 import networkx as nx
 
-alpha = 0.5
-beta = 1.2
+alpha = 1.1
+beta = 1.5
 
 
 class Ant:
@@ -15,14 +15,15 @@ class Ant:
         self.graph = graph
         self.begin = begin
         self.end = end
+        self.alive = True
         self.current_node = begin
 
-    def walk_to_goal(self):
+    def walk_to_goal_basic(self):
 
         path_walked = [self.current_node]
 
         while self.current_node != self.end:
-            solutions, probabilities = generate_solutions(self.graph, self.current_node)
+            solutions, probabilities = generate_solutions_basic(self.graph, self.current_node)
 
             solution = choice(solutions, p=probabilities)
 
@@ -40,6 +41,31 @@ class Ant:
             segment_length = self.graph[node][next_node]['distance']
             self.distance_walked += segment_length
 
+    def walk_to_goal_extended(self):
+        path_walked = [self.current_node]
+
+        while self.current_node != self.end:
+            solutions, probabilities = generate_solutions_extended(self.graph, self.current_node, path_walked)
+
+            if not solutions:
+                # ant has nowhere to go and has to die
+                self.alive = False
+                return
+
+            solution = choice(solutions, p=probabilities)
+
+            path_walked.append(solution)
+            self.current_node = solution
+
+        self.path_walked = path_walked
+
+        # calculating total distance walked by the ant (without loops)
+        for i in range(len(path_walked) - 1):
+            node = self.path_walked[i]
+            next_node = self.path_walked[i + 1]
+
+            segment_length = self.graph[node][next_node]['distance']
+            self.distance_walked += segment_length
         # mrowka konczy bieg
         # na przebytej drodze zostawiamy troszkę feromonu
         # informacja o przebytej drodze i jej odleglosci w polach klasy Ant
@@ -61,23 +87,51 @@ def remove_loops(path):
     return path
 
 
-def generate_solutions(graph, node):
+def generate_solutions_basic(graph, node):
     sum = 0
     for neighbor in graph.neighbors(node):
         pheromone = graph[node][neighbor]['pheromone']
-        distance = graph[node][neighbor]['distance']
-        sum += pheromone ** alpha * (1 / distance) ** beta
+        sum += pheromone ** alpha
 
     possible_solutions = []
     probabilities = []
 
     for neighbor in graph.neighbors(node):
         pheromone = graph[node][neighbor]['pheromone']
+        if sum != 0:
+            probability = (pheromone ** alpha) / sum
+        else:
+            probability = 1 / len(list(nx.neighbors(graph, node)))
+
+        possible_solutions.append(neighbor)
+        probabilities.append(probability)
+
+    return possible_solutions, probabilities
+
+
+def generate_solutions_extended(graph, node, visited_nodes):
+    sum = 0
+    allowed_neighbors = []
+
+    for neighbor in graph.neighbors(node):
+        if neighbor not in visited_nodes:
+            allowed_neighbors.append(neighbor)
+
+    for neighbor in allowed_neighbors:
+        pheromone = graph[node][neighbor]['pheromone']
+        distance = graph[node][neighbor]['distance']
+        sum += (pheromone ** alpha) * (1 / distance) ** beta
+
+    possible_solutions = []
+    probabilities = []
+
+    for neighbor in allowed_neighbors:
+        pheromone = graph[node][neighbor]['pheromone']
         distance = graph[node][neighbor]['distance']
         if sum != 0:
             probability = pheromone ** alpha * (1 / distance ** beta) / sum
         else:
-            probability = 1 / len(list(nx.neighbors(graph, node)))
+            probability = 1 / len(allowed_neighbors)
 
         possible_solutions.append(neighbor)
         probabilities.append(probability)
--- ant_colony.py
+++ ant_colony.py
@@ -2,13 +2,14 @@ import networkx as nx
 from loader import load_graph
 from operator import itemgetter
 from Ant import Ant
-
+from dijkstry import dijkstry
+from matplotlib import pyplot as plt
 
 NUMBER_OF_ITERATIONS = 200
-NUMBER_OF_ANTS = 10
+NUMBER_OF_ANTS = 100
 
-PHEROMONE_COEFFICIENT = 3
-EVAPORATION_COEFFICIENT = 0.2
+PHEROMONE_COEFFICIENT = 5
+EVAPORATION_COEFFICIENT = 0.25
 
 
 def simulate_colony():
@@ -18,9 +19,16 @@ def simulate_colony():
     begin = min(graph.nodes())
     end = max(graph.nodes())
 
-    for _ in range(NUMBER_OF_ITERATIONS):
+    most_common_paths = []
+    most_common_paths_counts = []
+    iterations = []
+
+    for iteration in range(NUMBER_OF_ITERATIONS):
 
         ants = []
+
+        all_paths = []
+        all_paths_counts = []
 
         # add N ants
         for ant_id in range(NUMBER_OF_ANTS):
@@ -29,33 +37,72 @@ def simulate_colony():
 
         # let the ants start their journey
         for ant in ants:
-            ant.walk_to_goal()
+            ant.walk_to_goal_extended()
 
         for ant in ants:
-            delta_pheromone = PHEROMONE_COEFFICIENT / ant.distance_walked
+            if ant.alive:
+                delta_pheromone = PHEROMONE_COEFFICIENT / ant.distance_walked
 
-            for i in range(len(ant.path_walked) - 1):  # for each segment
-                node = ant.path_walked[i]
-                next_node = ant.path_walked[i + 1]
+                if (ant.path_walked, ant.distance_walked) in all_paths:
+                    all_paths_counts[all_paths.index((ant.path_walked, ant.distance_walked))] += 1
+                else:
+                    all_paths.append((ant.path_walked, ant.distance_walked))
+                    all_paths_counts.append(1)
 
-                graph[node][next_node]['pheromone'] += delta_pheromone
+                for i in range(len(ant.path_walked) - 1):  # for each segment
+                    node = ant.path_walked[i]
+                    next_node = ant.path_walked[i + 1]
+
+                    graph[node][next_node]['pheromone'] += delta_pheromone
 
         for edge in graph.edges:
 
-            graph[edge[0]][edge[1]]['pheromone'] *= (1 - PHEROMONE_COEFFICIENT)
+            graph[edge[0]][edge[1]]['pheromone'] *= (1 - EVAPORATION_COEFFICIENT)
 
             # edge['pheromone'] *= (1 - PHEROMONE_COEFFICIENT)
 
             if graph[edge[0]][edge[1]]['pheromone'] < 0:
                 graph[edge[0]][edge[1]]['pheromone'] = 0
 
+        max_index = all_paths_counts.index(max(all_paths_counts))
+
+        # all_paths = sorted(zip(all_paths, all_paths_counts), key=lambda k: k[1], reverse=True)
+
+        most_common_paths.append(all_paths[max_index])
+        most_common_paths_counts.append(all_paths_counts[max_index])
+        iterations.append(iteration)
+
     determinant_ant = Ant(-1, graph, begin, end)
 
-    determinant_ant.walk_to_goal()
+    determinant_ant.walk_to_goal_extended()
 
     final_path = determinant_ant.path_walked
 
     print(final_path)
+    print(determinant_ant.distance_walked)
+
+    distances = [dist for _, dist in most_common_paths]
+
+    plt.plot(iterations, distances)
+    plt.plot(iterations, most_common_paths_counts)
+    plt.savefig('./figure.png')
+
+    nx.set_node_attributes(graph, None, 'parent')
+    nx.set_node_attributes(graph, False, 'visited')
+    new_graph = dijkstry(graph, 0, 19)
+    dijkstra_path = []
+    for node in new_graph.nodes:
+        # print(node, end=' ')
+        dijkstra_path.append(node)
+    total_distance = 0
+    for i in range(len(dijkstra_path) - 1):  # for each segment
+        node = dijkstra_path[i]
+        next_node = dijkstra_path[i + 1]
+
+        distance = graph[node][next_node]['distance']
+        total_distance += distance
+    print(dijkstra_path[::-1])
+    print(total_distance)
 
 
 def ant_colony_algorithm(graph, begin, end):  # for each ant
